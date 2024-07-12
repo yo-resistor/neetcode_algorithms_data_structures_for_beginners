bec597d637c27f8dbf5a22f06c359e6331018268
Yunsik Ohm <yunsikohm@gmail.com>
2024-07-12 13:36:05 -0700

    practiced matrix dfs with leetcode 200

--- 29_matrix_dfs/200_number_of_islands.py
+++ 29_matrix_dfs/200_number_of_islands.py
@@ -1,6 +1,31 @@
 class Solution:
     def numIslands(self, grid: list[list[str]]) -> int:
-        return 0
+        ROWS, COLS = len(grid), len(grid[0])
+        visit = set()
+        
+        def dfs(r, c):
+            if (
+                min(r, c) < 0 or
+                r == ROWS or c == COLS or
+                grid[r][c] == "0" or
+                (r, c) in visit
+            ):
+                return
+            
+            visit.add((r, c))
+            
+            neighbors = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+            for dr, dc in neighbors:
+                dfs(r + dr, c + dc)
+            
+        islands = 0
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == "1" and (i, j) not in visit:
+                    islands += 1
+                    dfs(i, j)
+        
+        return islands
     
 def main():
     sol = Solution()
