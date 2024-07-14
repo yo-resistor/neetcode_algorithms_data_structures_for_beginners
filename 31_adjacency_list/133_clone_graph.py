6efc36d6a059b101fce11f045106ef3775530b92
Yunsik Ohm <yunsikohm@gmail.com>
2024-07-14 13:07:17 -0700

    practiced adjacency list with leetcode 133

---
+++ 31_adjacency_list/133_clone_graph.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Node) -> Node:
+        oldToNew = dict()
+        
+        def dfs(node):
+            if node in oldToNew:
+                return oldToNew[node]
+            
+            new = Node(node.val)
+            oldToNew[node] = new
+            for neighbor in node.neighbors:
+                new.neighbors.append(neighbor)
+                
+            return new
+        return dfs(node) if node else None
